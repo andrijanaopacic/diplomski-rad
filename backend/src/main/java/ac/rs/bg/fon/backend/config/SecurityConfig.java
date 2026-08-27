347d4d59ae3a295589871f11cbf42d2e58a6251a
unknown <andrijana.opacic04@gmail.com>
2026-08-27 21:43:33 +0200

    Dodatо povezivanje sa vercel app

--- backend/src/main/java/ac/rs/bg/fon/backend/config/SecurityConfig.java
+++ backend/src/main/java/ac/rs/bg/fon/backend/config/SecurityConfig.java
@@ -66,7 +66,10 @@ public class SecurityConfig {
 	
 	private CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration cfg = new CorsConfiguration();
-		cfg.setAllowedOrigins(List.of("http://localhost:3000"));
+		cfg.setAllowedOrigins(List.of(
+			    "http://localhost:3000",
+			    "https://diplomski-rad-79w8.vercel.app"
+			));
 		cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 		cfg.setAllowedHeaders(List.of("*"));
 		cfg.setAllowCredentials(true);
